d511a5a752d4cc555fbf1bfdb9937d8a89daeeea
justus <jmbaluka@mfarm.co.ke>
2015-07-05 15:01:37 +0300

    added some typed js on it

--- justus.js
+++ justus.js
@@ -1,1 +1,25 @@
  $(".button-collapse").sideNav();
+
+ $(function(){
+
+        $("#typed").typed({
+            strings: ["Hello friend.", "It is quite nice of you to pass by today.", "I am still building this house of mine though.", "It's coming soon!"],
+            typeSpeed: 40,
+            backDelay: 600,
+            loop: false,
+            contentType: 'html', // or text
+            // defaults to false for infinite loop
+            loopCount: false,
+            callback: function(){ foo(); },
+            resetCallback: function() { newTyped(); }
+        });
+
+        $(".reset").click(function(){
+            $("#typed").typed('reset');
+        });
+
+    });
+
+    function newTyped(){ /* A new typed object */ }
+
+    function foo(){ console.log("Callback"); }
